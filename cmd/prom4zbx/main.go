6fe6679fcc9703e79df6a01e2a602f066a985aab
Gushchin Andrei <neogan74@gmail.com>
2020-01-23 23:06:19 +0500

    Add flags for additional parameters

--- cmd/prom4zbx/main.go
+++ cmd/prom4zbx/main.go
@@ -1,16 +1,23 @@
 package main
 
 import (
+	"flag"
+
 	"prom2zbx.com/internal/prom"
-	"prom2zbx.com/internal/alerts"
 )
 
 func main() {
-	// prom.GetTargetsProm2LLD()
-	prom.GetRules()
-
-
-	alerts.ListenAlerts()
+	promURL := flag.String("promURL", "", "Prometheus URL")
+	mode := flag.String("mode", "targets", "Mode: targets or rules")
+	prefix := flag.String("prefix", "TEST", "Prefix for avoid duplicates")
+	flag.Parse()
+	switch *mode {
+	case "targets":
+		prom.GetTargetsProm2LLD(*promURL, *prefix)
+	case "rules":
+		prom.GetRules(*promURL)
+	}
+	// alerts.ListenAlerts()
 	// _, err := http.NewRequest("POST", "http://127.0.0.1:10055", strings.NewReader(alertOK))
 	// if err != nil {
 	// 	fmt.Errorf("%v", err)
