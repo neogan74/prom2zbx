eebf345a934f2ac985181bed5779c7d47e5330ba
Gushchin Andrei <neogan74@gmail.com>
2020-01-28 14:10:57 +0500

    client for testing

--- client/client.go
+++ client/client.go
@@ -1,51 +1,65 @@
 package main
 
 import (
-	"log"
+	"fmt"
+	"io/ioutil"
 	"net/http"
 	"strings"
+	"time"
 )
 
-const alertOK = `{  
-	"version":"4",
-	"groupKey":"{}:{alertname=\"InstanceDown\"}",
-	"status":"resolved",
-	"receiver":"testing",
-	"groupLabels":{  
-	   "alertname":"InstanceDown"
-	},
-	"commonLabels":{  
-	   "alertname":"InstanceDown",
-	   "instance":"abc",
-	   "job":"node_exporter",
-	   "severity":"critical"
-	},
-	"commonAnnotations":{  
-	   "description":"localhost:9100 of job node_exporter has been down for more than 1 minute.",
-	   "summary":"Instance localhost:9100 down"
-	},
-	"externalURL":"http://edas-GE72-6QC:9093",
-	"alerts":[  
-	   {  
-		  "labels":{  
-			 "alertname":"InstanceDown",
-			 "instance":"localhost:9100",
-			 "job":"node_exporter",
-			 "severity":"critical"
-		  },
-		  "annotations":{  
-			 "description":"localhost:9100 of job node_exporter has been down for more than 1 minute.",
-			 "summary":"Instance localhost:9100 down"
-		  },
-		  "startsAt":"2018-08-30T16:59:09.653872838+03:00",
-		  "EndsAt":"2018-08-30T17:01:09.656110177+03:00"
-	   }
-	]
- }`
-
 func main() {
-	_, err := http.NewRequest("POST", "http://127.0.0.1:3000", strings.NewReader(alertOK))
+
+	url := "http://localhost:9094"
+	method := "POST"
+
+	payload := strings.NewReader("{\n    \"version\": \"4\",\n    \"groupKey\": \"{")
+	/* }:{alertname=\\\"InstanceDown\\\"}\",\n
+	\"status\": \"resolved\",\n
+	\"receiver\": \"testing\",\n
+	\"groupLabels\": {\n
+	\"alertname\": \"InstanceDown\"\n
+	},\n
+	\"commonLabels\": {\n
+		\"alertname\": \"InstanceDown\",\n
+		\"instance\": \"abc\",\n
+		\"job\": \"node_exporter\",\n
+		\"severity\": \"critical\"\n
+	},\n
+	\"commonAnnotations\": {\n
+			\"description\": \"localhost:9100 of job node_exporter has been down for more than 1 minute.\",\n
+			\"summary\": \"Instance localhost:9100 down\"\n
+	},\n
+	\"externalURL\": \"http://edas-GE72-6QC:9093\",\n
+	\"alerts\": [\n
+		{\n
+			\"labels\": {\n
+			\"alertname\": \"InstanceDown\",\n
+			\"instance\": \"RMRUMOWLSPBIAS5.web.rru02.vwgroup.com\",\n
+			\"job\": \"node_exporter\",\n
+			\"severity\": \"critical\"\n
+		},\n
+		\"annotations\": {\n
+			\"description\": \"localhost:9100 of job node_exporter has been down for more than 1 minute.\",\n
+			\"summary\": \"Instance localhost:9100 down\"\n
+		},\n
+		\"startsAt\": \"2018-08-30T16:59:09.653872838+03:00\",\n
+		\"EndsAt\": \"2018-08-30T17:01:09.656110177+03:00\"\n
+		}\n
+	]\n}") */
+
+	time.Sleep(time.Second * 10)
+	client := &http.Client{}
+	req, err := http.NewRequest(method, url, payload)
+
 	if err != nil {
-		log.Fatalf("%v", err)
+		fmt.Println(err)
 	}
+	req.Header.Add("Content-Type", "application/json")
+
+	res, err := client.Do(req)
+	defer res.Body.Close()
+	body, err := ioutil.ReadAll(res.Body)
+
+	fmt.Println(string(body))
 }
